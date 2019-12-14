fc493794c0c75f40c451f42a39bd8cfb48d7fcd7
Sandeep Mahajan <rajesh.yadav@consagous.com>
2019-12-14 16:27:34 +0530

    Bridgin header file updated

--- SandeepIndoreDemo/SandeepIndoreDemo-Bridging-Header.h
+++ SandeepIndoreDemo/SandeepIndoreDemo-Bridging-Header.h
@@ -2,13 +2,12 @@
 //  SandeepIndoreDemo-Bridging-Header.h
 //  SandeepIndoreDemo
 //
-//  Created by consagous on 14/12/19.
 //
 
 #ifndef SandeepIndoreDemo_Bridging_Header_h
 #define SandeepIndoreDemo_Bridging_Header_h
 
 
-#endif /* SandeepIndoreDemo_Bridging_Header_h */
+#endif 
 
 @import SDWebImage;
